c31244dc79fb1034601159abf511edc2889f1a78
PEDENAUD <LAURA>
2023-06-11 18:20:51 +0200

    show movies from the database

--- frontend/src/app/api_calls/movie.service.ts
+++ frontend/src/app/api_calls/movie.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class MovieService {
-  private baseUrl = 'http://localhost:8080/movie';
+  private baseUrl = "http://localhost:8080";
   constructor(private http: HttpClient) {}
 
   getPopularMovies(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/popular`);
+    return this.http.get<any>(`${this.baseUrl}/film`);
   }
 }
--- frontend/src/app/types.ts
+++ frontend/src/app/types.ts
@@ -1,16 +1,4 @@
 export interface Movie {
-  adult: boolean;
-  backdropPath: string;
-  genreIds: number[];
   id: number;
-  originalLanguage: string;
-  originalTitle: string;
-  overview: string;
-  popularity: number;
-  posterPath: string;
-  releaseDate: string;
-  title: string;
-  video: boolean;
-  voteAverage: number;
-  voteCount: number;
+  name: string;
 }
